e20fa58fa6eac7514d421073a2bdbd6328e184da
Victor Panayotov <victorpanayotov@gmail.com>
2024-06-09 21:48:49 +0100

    Add iced, render empty window

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,31 @@
-fn main() {
-    println!("Hello, world!");
+use iced::executor;
+use iced::{Application, Command, Element, Settings, Theme};
+
+pub fn main() -> iced::Result {
+    App::run(Settings::default())
+}
+
+struct App;
+
+impl Application for App {
+    type Executor = executor::Default;
+    type Flags = ();
+    type Message = ();
+    type Theme = Theme;
+
+    fn new(_flags: ()) -> (App, Command<Self::Message>) {
+        (App, Command::none())
+    }
+
+    fn title(&self) -> String {
+        String::from("Fractal renderer")
+    }
+
+    fn update(&mut self, message: Self::Message) -> Command<Self::Message> {
+        Command::none()
+    }
+
+    fn view(&self) -> Element<'_, Self::Message, Self::Theme, iced::Renderer> {
+        "Fractal goes here".into()
+    }
 }
