9237353267dbeaa6f43101babe95e047b94fae2c
Victor Panayotov <victorpanayotov@gmail.com>
2024-06-10 02:02:49 +0100

    More complex distance function

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,11 @@
-use glam::{vec3, Vec3};
+use glam::{vec3, Vec3, Vec3Swizzles};
 use iced::executor;
 use iced::{Application, Command, Element, Settings, Theme};
 
 use iced::widget::image;
 
-const IMG_H: usize = 500;
-const IMG_W: usize = 1000;
-const IMG_TEST1: [u8; 4 * IMG_H * IMG_W] = [100; 4 * IMG_H * IMG_W];
+const IMG_H: usize = 750;
+const IMG_W: usize = 750;
 
 pub fn main() -> iced::Result {
     App::run(Settings::default())
@@ -34,7 +33,7 @@ impl Application for App {
 
     fn view(&self) -> Element<'_, Self::Message, Self::Theme, iced::Renderer> {
         let pixels = trace_image(
-            vec3(2.0, 0.0, 0.0),
+            vec3(1.0, 1.0, 3.75),
         );
 
         let handle = image::Handle::from_pixels(
@@ -47,12 +46,12 @@ impl Application for App {
     }
 }
 
-const MAX_STEPS: i16 = 20;
+const MAX_STEPS: i16 = 100;
 const MIN_DISTANCE: f32 = 0.0001;
 
 fn trace_image(camera_position: Vec3) -> Vec<u8> {
     // For now, "texture" grid will be 1units x 1units, "located" 1unit in front of the camera
-    let camera_direction = vec3(-1.0, 0.0, 0.0);
+    let camera_direction = vec3(0.0, 0.0, -1.0);
 
     let mut buffer: Vec<u8> = Vec::with_capacity(4 * IMG_W * IMG_H);
     
@@ -60,9 +59,9 @@ fn trace_image(camera_position: Vec3) -> Vec<u8> {
     for i in 0..IMG_W {
         for j in 0..IMG_H {
             let direction = camera_direction + vec3(
-                0.0,
                 (i as i32 - IMG_W as i32/2) as f32 / IMG_W as f32,
-                (j as i32 - IMG_H as i32/2) as f32 / IMG_H as f32
+                (j as i32 - IMG_H as i32/2) as f32 / IMG_H as f32,
+                0.0,
             );
 
             // Trace the ray, compute a colour, store in buffer
@@ -95,5 +94,10 @@ fn trace(from: Vec3, direction: Vec3) -> f32{
 }
 
 fn sdf(point: Vec3) -> f32 {
-    return point.length() - 0.5;
+    let x = point.x % 1.0;
+    let y = point.y % 1.0;
+
+    let instance = vec3(x, y, point.z) - vec3(0.5, 0.5, 0.5);
+
+    return instance.length() - 0.4;
 }
